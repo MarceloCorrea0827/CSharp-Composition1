36f1ce24c28b416fa52a8fec91915279a6427f4d
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-12 20:19:46 -0300

    Instanciando Classes Worker e  Department, e Enum WorkerLevel

---
+++ Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Composition1.Entities
+{
+    class Department
+    {
+        public String Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Composition1.Entities;
+using Composition1.Entities.Enums;
+
+namespace Composition1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter department's name: ");
+            string deptName = Console.ReadLine();
+
+            Console.WriteLine("Enter Worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department department = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, department);
+        }
+    }
+}
