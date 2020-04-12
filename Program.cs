dfb1305c9e57e45ea3cc2f34216b88425d258b3c
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-12 20:54:01 -0300

    Adicionando os Contratos de Trabalho (Classe HourContract) ao Funcionario (Classe Worker)

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace Composition1
     {
         static void Main(string[] args)
         {
+            // Nome do Departamento
             Console.WriteLine("Enter department's name: ");
             string deptName = Console.ReadLine();
 
+            // Recebendo Dados iniciais do Funcionário
             Console.WriteLine("Enter Worker data:");
             Console.Write("Name: ");
             string name = Console.ReadLine();
@@ -20,8 +22,31 @@ namespace Composition1
             Console.Write("Base salary: ");
             double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            // Composição entre as classe Worker e Department
             Department department = new Department(deptName);
             Worker worker = new Worker(name, level, baseSalary, department);
+
+            // Ler os contratos do Funcionário(Worker)
+            Console.WriteLine("How many contracts to this Worker?: ");
+            int numContracts = int.Parse(Console.ReadLine());
+
+            for (int i=1; i < numContracts; i++)
+            {
+                // Dados do Contrato
+                Console.WriteLine($"Enter #{i} contract data"); //Interpolação
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Durations (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                // Instanciando Contract
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+
+                // Adicionando este Contrato de trabalho ao Funcionário (Worker)
+                worker.AddContract(contract);
+            }
         }
     }
 }
